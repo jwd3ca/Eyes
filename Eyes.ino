90e369a747589676a885d8ed117a970bf1371668
J. Davis <jwd3ca@gmail.com>
2025-02-16 10:26:34 -0800

    Initial commit

---
+++ Eyes.ino
@@ -0,0 +1,203 @@
+// Just test fullscreen sprite M5Core2
+
+#include <M5Unified.h>
+#include <Adafruit_GFX.h>  // Needed for sprites! Ensure this is included
+#include <HardwareSerial.h>
+#include <Adafruit_SHT31.h>
+#include "config.h"
+
+// Create an SHT31 object
+Adafruit_SHT31 sht30 = Adafruit_SHT31();
+
+// WIFI_SSID, WIFI_PASSWORD, influxdb credentials, etc. all defined in github-hidden config file
+
+#define MyFont &FreeSansBold24pt7b
+
+#define PMS_RX_PIN 13  // RX from PMS5003
+#define PMS_TX_PIN 14  // TX from PMS5003
+
+HardwareSerial pms5003(2);  // Serial2 for PMS5003
+uint8_t buffer[32];         // Buffer to store incoming data
+
+#define DATA_LEN 32
+
+LGFX_Sprite EyesSprite = LGFX_Sprite(&M5.Display);
+
+const unsigned long eventTime_circle = 100;
+unsigned long previousTime = 0;
+
+// Blink timing
+unsigned long nextBlinkTime = 0;
+bool isBlinking = false;
+unsigned long blinkStartTime = 0;
+const unsigned long blinkDuration = 250;  // Total blink duration
+  //: 0-3, 0 is best, 3 is bad
+
+int pm1_0 = 0;
+
+int iterations = 0;
+
+float temperature = 0;
+float humidity = 0;
+int pupilColor = 0x000000;
+
+//===================================================================
+void setup() {
+  auto cfg = M5.config();
+
+  M5.begin(cfg);
+  Serial.begin(115200);  // Must do this here for M5Unified!
+  delay(2000);
+
+  Serial.println("M5Unified Initialized!");
+  delay(500);
+
+  // Try creating a 320x240 sprite
+  EyesSprite.setPsram(true);          // force using psram
+  EyesSprite.createSprite(320, 240);  // Create sprite buffer
+  Serial.printf("Sprite Width: %d, Height: %d\n", EyesSprite.width(), EyesSprite.height());
+  Serial.printf("Total PSRAM: %d, Free PSRAM: %d\n", ESP.getPsramSize(), ESP.getFreePsram());
+
+  // Initialize PMS5003 serial communication
+  pms5003.begin(9600, SERIAL_8N1, PMS_RX_PIN, PMS_TX_PIN);
+  Serial.println("PMS5003 initialized!");
+
+
+  // Initialize I²C explicitly with the Core2 default pins:
+  Wire.begin(21, 22);  // SDA=21, SCL=22
+
+  // Initialize the SHT30 sensor at address 0x44
+  if (!sht30.begin(0x44)) {
+    Serial.println("Couldn't find SHT30 sensor at 0x44");
+    while (1) {
+      delay(1);
+    }
+  }
+}
+
+//====================================================================
+void loop() {
+  int comfortLevel;
+
+  readPMSData();
+  // Serial.printf("PM1_0 : %d µg/m³\n", pm1_0);
+
+  get_SHT30data();
+  // Serial.printf("temperature: %.2f, humidity: %.2f\n",temperature, humidity );
+
+  // comfortLevel: 0-3, 0 is best, 3 is bad
+
+  // pm1_0 = 40;
+
+  if (pm1_0 > 151) {
+    comfortLevel = 3;
+    pupilColor = TFT_RED;
+  } else if (pm1_0 > 81) {
+    comfortLevel = 2;
+    pupilColor = TFT_YELLOW;
+  } else if (pm1_0 > 31) {
+    comfortLevel = 1;
+    pupilColor = TFT_DARKGREEN;
+  } else {
+    comfortLevel = 0;
+    pupilColor = TFT_BLUE;
+  }
+
+  EyesSprite.fillSprite(TFT_BLACK);  // Clear screen before drawing
+  updateDisplay(comfortLevel);
+  EyesSprite.pushSprite(0, 0);  // Update display
+}
+
+//---------------------------------------------------------------------
+void updateDisplay(int comfortLevel) {
+  unsigned long currentMillis = millis();
+
+
+  if (currentMillis >= nextBlinkTime && !isBlinking) {
+    isBlinking = true;
+    blinkStartTime = currentMillis;
+    nextBlinkTime = currentMillis + random(3000, 7000);
+  }
+
+  if (isBlinking && (currentMillis - blinkStartTime >= blinkDuration)) {
+    isBlinking = false;
+  }
+
+  int eyelidHeight = isBlinking ? 120 : comfortLevel * 40;
+
+  if (comfortLevel == 3) eyelidHeight = 85;  // Almost closed
+
+  drawEyes(eyelidHeight);
+}
+
+//---------------------------------------------------------------------
+void drawEyes(int eyelidHeight) {
+  const int leftEyeX = 94;
+  const int rightEyeX = 222;
+  const int eyeY = 128;
+  const int eyeRadius = 60;
+  const int pupilSize = 25;
+  const int eyeVerticalRadius = eyeRadius * 1.6;  // Make eyes longer vertically
+
+  EyesSprite.fillEllipse(leftEyeX, eyeY, eyeRadius, eyeVerticalRadius, 0x61c4db);
+  EyesSprite.fillEllipse(rightEyeX, eyeY, eyeRadius, eyeVerticalRadius, 0x61c4db);
+
+  // pupils
+
+  // Draw left pupil slightly to the right of the center for cartoonish effect
+  EyesSprite.fillCircle(leftEyeX + 10, eyeY, pupilSize, pupilColor);
+  // Draw right pupil slightly to the left of the center for cartoonish effect
+  EyesSprite.fillCircle(rightEyeX - 10, eyeY, pupilSize, pupilColor);
+
+  // Top of the eyelid is based on the vertical radius
+  int eyeTop = eyeY - eyeVerticalRadius;
+  EyesSprite.fillRect(0, eyeTop - 30, 340, eyelidHeight, TFT_BLACK);
+}
+
+//===================================================================
+static bool get_SHT30data() {
+  // Read temperature in °C and relative humidity in %
+  temperature = sht30.readTemperature();
+  // the proximity to the base seems to result in high temp reading. Reduce by some % here...
+  temperature *= 0.820;
+  humidity = sht30.readHumidity();
+
+  // Check if readings are valid (not NaN)
+  if (!isnan(temperature) && !isnan(humidity)) {
+    return (true);
+  } else {
+    Serial.println("Failed to read from SHT30 sensor!");
+    return (false);
+  }
+}
+
+//===================================================================
+// Function to read PMS5003 data
+bool readPMSData() {
+  uint8_t buffer[DATA_LEN];
+  int16_t values[16];
+
+  if (pms5003.available() >= DATA_LEN) {
+    int bytesRead = pms5003.readBytes(buffer, DATA_LEN);
+
+    if (bytesRead == DATA_LEN && buffer[0] == 0x42 && buffer[1] == 0x4D) {
+      // Process data (16-bit values in Big Endian format)
+      for (int i = 0, j = 0; i < DATA_LEN; i += 2, j++) {
+        values[j] = (buffer[i] << 8) | buffer[i + 1];
+      }
+
+      // Assign values to variables
+      pm1_0 = values[5];
+      /*
+      pm2_5 = values[6];
+      pm10 = values[7];
+      point3sum = values[8];
+      point5sum = values[9];
+      point10sum = values[10];
+      */
+
+      return true;
+    }
+  }
+  return false;
+}
---
+++ Readme.md
@@ -0,0 +1,6 @@
+Code to use cartoonish eyes to indicate the state of air qualtiy, temp and humidity.
+Uses M5Stack PMSA003 unit sitting on the M5Stack SHT30 base stand.
+
+Uses wifi/influxdb to post data to local influxdb server which is then read by local grafana server
+to display historical, as well as current, data. localhost:3000
+
